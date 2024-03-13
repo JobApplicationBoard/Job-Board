9293b6c7bcea182ed7c7f60ad65f664ce12cc027
Stephen <stephenchow1111@gmail.com>
2024-03-14 04:49:28 +0800

    added login router and login userController logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,20 +2,57 @@ const db = require('../models/jobModels');
 
 const userController = {};
 
-userController.getUser = (req,res,next) => {
-
-};
+userController.getUser = (req, res, next) => {};
 
 userController.createUser = (req, res, next) => {
-    
-}
+  const { username, password } = req.body;
+  const query = `
+  INSERT INTO users (username, password)
+  VALUES ($1, $2) 
+  RETURNING *
+  `;
 
-userController.verifyUser = (req,res,next) => {
+  db.query(query, [username, password])
+    .then((result) => {
+      console.log('I am result from database: ', result);
+      res.locals.newUser = result.rows[0];
+      return next();
+    })
+    .catch((err) => {
+      return next({
+        log: 'userController.createUser: ERROR',
+        err,
+        message: {
+          err: 'Error occurred in userController.createUser. Check server logs for more details.',
+        },
+      });
+    });
+};
 
-}
+userController.login = (req, res, next) => {
+    const { username, password } = req.body;
+    const query = 'SELECT * FROM users WHERE username = $1';
+  
+    db.query(query, [username])
+      .then((result) => {
+        if (result.rows.length === 0 || password !== result.rows.password) {
+          return res.status(401).json({ error: 'Cannot find user or invalid password' });
+        }
+        const user = result.rows[0];
+        res.locals.user = user
+        return next();
+      })
+      .catch((err) => {
+        return next({
+          log: `Error in userController.login: ${err}`,
+          status: 500,
+          message: { err: 'An error occurred during login process' },
+        });
+      });
+  };
 
-userController.deleteUser = (req, res, next) => {
-    
-}
+userController.verifyUser = (req, res, next) => {};
+
+userController.deleteUser = (req, res, next) => {};
 
 module.exports = userController;
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -4,25 +4,27 @@ const userController = require('../controllers/userController');
 
 const userRouter = express.Router();
 
-userRouter.get('/login', (req, res) =>{
+// userRouter.get('/login', (req, res) => {
+//   // load structure of login page
+//   res.status(200).json('Login successful', res.locals.user)
+// });
+
+userRouter.post('/login', userController.login, (req, res) => {
   // load structure of login page
-
+  res.status(200).json({ message: 'Login successful', user: res.locals.user });
 });
 
-userRouter.post('/login',userController.verifyUser, (req,res) => {
-  // post request for logging in 
+userRouter.post('/login', userController.verifyUser, (req, res) => {
+  // post request for logging in
 });
 
-userRouter.get('/register', (req, res) =>{
+userRouter.get('/register', (req, res) => {
   // load structure of register page
-
 });
 
-userRouter.post('/register',userController.createUser, (req,res) => {
-  // post request for registering 
-  
+userRouter.post('/register', userController.createUser, (req, res) => {
+  // post request for registering
+  res.status(200).json(res.locals.newUser);
 });
-
-
 
 module.exports = userRouter;
