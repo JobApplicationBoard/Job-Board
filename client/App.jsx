2512f23b8d85961a2907dadaf2e788fbca380a0e
Evan Griffith <evan@evangriffith.net>
2024-03-14 08:09:21 -0700

    Passed setState through to App.jsx

--- client/App.jsx
+++ client/App.jsx
@@ -9,30 +9,23 @@
  * ************************************
  */
 
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Board from '../client/components/Board.jsx';
 import Login from './components/Login.jsx';
 
-
-
 const App = () => {
-<<<<<<< HEAD
   const [renderLogin, setRenderLogin] = useState(true);
-  
-=======
->>>>>>> dev
 
   return (
     <div>
       <header>
         <div class="title-container">
-          
           <h1 className="head-title">Axojobl - Navigate Your Career Waters</h1>
         </div>
       </header>
       <div className="app-container">
-        { renderLogin ? <Login /> : <Board /> }
+        {renderLogin ? <Login setRenderLogin={setRenderLogin} /> : <Board />}
       </div>
     </div>
   );
